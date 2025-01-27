f208a2f0904372cfcf1f28bd19cda9026c65d43a
Roni <64074505+RonishRohan@users.noreply.github.com>
2025-01-27 17:55:52 +0530

    Switched to accordions

---
+++ src/components/Accordion.tsx
@@ -0,0 +1,33 @@
+import React, { useState, type ReactNode } from "react";
+
+const Accordion = ({title, children} : {title : string, children: ReactNode}) => {
+  const [open, setOpen] = useState(false);
+  return (
+    <div className="flex flex-col w-full h-fit">
+      <button
+        onClick={() => setOpen((prev) => !prev)}
+        className={`hover:underline cursor-pointer relative flex justify-start transition-all duration-75 ${open && "italic font-bold"} hover:font-bold hover:text-white`}
+      >
+        <div className="absolute right-full mx-2 h-full size-2 flex items-center justify-center">
+          <div className="h-[1px] w-full absolute bg-white"></div>
+          <div
+            style={{ rotate: !open ? "-90deg" : "0deg" }}
+            className="h-[1px] w-full absolute duration-100 ease-in-out transition-all bg-white"
+          ></div>
+        </div>
+        {title}
+      </button>
+
+      <>
+        <div
+          style={{ height: open ? "auto" : "0px", paddingBottom: open ? "1rem" : "0px" }}
+          className={`transition-[height] duration-500 ease-in-out w-full overflow-hidden`}
+        >
+          {children}
+        </div>
+      </>
+    </div>
+  );
+};
+
+export default Accordion;
