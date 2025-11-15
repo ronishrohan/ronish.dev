4eede574825539bd32ac502d74d38282d0a63e28
ronish <ronish.rohan@gmail.com>
2025-11-16 02:10:19 +0530

    feat: add navbar with button

--- src/components/navbar/navbar.svelte
+++ src/components/navbar/navbar.svelte
@@ -1,5 +1,9 @@
-<div class="fixed w-[100vw]  items-center left-0 top-0 h-[64px] flex gap-2 border-b border-b-2 border-black">
+<div class="fixed w-[100vw]  items-center left-0 top-0 h-[64px] flex gap-2 border-b border-b-2 border-stone-300">
     <div class="max-w-[1200px] w-full mx-auto p-4 flex items-center" >
-        <div class="font-bold text-lg">My SvelteKit App</div>
+        <div class="font-light text-lg  font-signature ">ronish</div>
+
+        <div class="ml-auto tracking-tight h-full flex gap-2 items-center " >
+            <button class="px-4 h-[40px] text-sm py-2  hover:bg-stone-200 hover:black text-stone-900 bg-gradient-to-t from-white to-stone-200 cursor-pointer border  border-stone-200 rounded-full shadow-[inset_0px_2px_2px_rgba(255,255,255,1),inset_0px_-2px_2px_rgba(0,0,0,0.1),0px_1px_2px_rgba(0,0,0,0.3)]" >About</button>
+        </div>
     </div>
 </div>
