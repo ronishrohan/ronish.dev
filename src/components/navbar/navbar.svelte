b3de96e033a0b01507e7bdb153bde6d607a8bd7a
ronish <ronish.rohan@gmail.com>
2025-11-16 01:49:08 +0530

    feat: started work on svelte site

---
+++ src/components/navbar/navbar.svelte
@@ -0,0 +1,5 @@
+<div class="fixed w-[100vw]  items-center left-0 top-0 h-[64px] flex gap-2 border-b border-b-2 border-black">
+    <div class="max-w-[1200px] w-full mx-auto p-4 flex items-center" >
+        <div class="font-bold text-lg">My SvelteKit App</div>
+    </div>
+</div>
