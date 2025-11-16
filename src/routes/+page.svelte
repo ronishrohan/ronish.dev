1dc6dc16b2c983f75ccf2193cbbd268bfd6e176d
ronish <ronish.rohan@gmail.com>
2025-11-17 00:15:31 +0530

    feat: add cat image

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,15 @@
-<div class="h-[90vh] pt-[80px] " >
-   <div>
-    i'm a developer and artist, currently pursuing my bachelors in engineering. i love designing and building websites, anything that has to do with colors, or typography or design. when I'm not coding, i'm probably watching movies, listening to music, or walking around the city.
 
+
+<div class="h-[90vh] pt-[80px] " >
+   <div class="w-full flex gap-2 items-stretch">
+    <div class="" >
+      hi i'm <span class="font-bold " >ronish rohan</span>, a software engineer at <span class="font-bold" >ghost</span>. <br><br>
+
+      i'm a developer and artist, currently pursuing my bachelors in engineering. i love designing and building websites, anything that has to do with colors, or typography or design. when I'm not coding, i'm probably watching movies, listening to music, or walking around the city.
+    </div>
+    <div class="w-1/3 shrink-0 relative rounded-2xl overflow-hidden " >
+      <img  src={"/cat.jpg"} class="absolute size-full left-0 top-0 object-cover" />
+   </div>
 
    </div>
 </div>
