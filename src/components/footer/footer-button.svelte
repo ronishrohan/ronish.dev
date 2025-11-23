3271f1f66ce853fd342d554adab82396432fe328
ronish <ronish.rohan@gmail.com>
2025-11-23 15:55:50 +0530

    feat: add footer links

--- src/components/footer/footer-button.svelte
+++ src/components/footer/footer-button.svelte
@@ -1,7 +1,8 @@
 <script>
     export let label;
+    export let href;
 </script>
 
-<button class="flex cursor-pointer items-center gap-1 hover:text-accent hover:underline">
+<a href={href} target="_blank" class="flex cursor-pointer items-center gap-1 hover:text-accent hover:underline">
 	{label}
-</button>
+</a>
--- src/components/footer/footer.svelte
+++ src/components/footer/footer.svelte
@@ -10,13 +10,13 @@
         <div class="w-fit shrink-0" >reach out to me,</div>
         <div  class="w-full text-left sm:text-center ">maybe i'll respond</div>
         <div class="w-fit shrink-0 flex gap-4" >
-            <FooterButton label="discord" />
-            <FooterButton label="instagram" />
-            <FooterButton label="mail" />
-            <div class="flex sm:hidden"><FooterButton label="github" /></div>
+            <FooterButton href="https://discord.gg/F9y8AqY5xE" label="discord" />
+            <FooterButton href="https://www.instagram.com/ronish2o/" label="instagram" />
+            <FooterButton href="mailto:ronish.rohan@gmail.com" label="mail" />
+            <div class="flex sm:hidden"><FooterButton href="https://github.com/ronishrohan" label="github" /></div>
         </div>
-        <button class="px-2 cursor-pointer ml-6 shadow-[inset_0px_2px_2px_rgba(255,255,255,0.4),inset_0px_-2px_2px_rgba(0,0,0,0.4),0px_1px_2px_rgba(0,0,0,0.3)] bg-gradient-to-t from-zinc-700 gap-2  to-zinc-800 rounded-full hidden sm:flex items-center hover:from-zinc-700 hover:to-zinc-700 transition-all duration-300" >
+        <a target="_blank" href="https://github.com/ronishrohan" class="px-2 cursor-pointer ml-6 shadow-[inset_0px_2px_2px_rgba(255,255,255,0.4),inset_0px_-2px_2px_rgba(0,0,0,0.4),0px_1px_2px_rgba(0,0,0,0.3)] bg-gradient-to-t from-zinc-700 gap-2  to-zinc-800 rounded-full hidden sm:flex items-center hover:from-zinc-700 hover:to-zinc-700 transition-all duration-300" >
             <GithubLogo weight="fill" size={14} />
-        </button>
+        </a>
     </div>
 </div>
